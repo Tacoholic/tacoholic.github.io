95d54dea63031604621c9cc55853e2ebecee41c5
Tacoholic <solischristian@gmail.com>
2019-11-12 21:59:32 -0600

    added logic for tetrominos so that it won't collide

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -2,7 +2,7 @@
 import { useState, useCallback } from 'react';
 
 import { TETROMINOS, randomTetromino } from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 //Thanks to ES6, we don't have to use whats below: 
     //const playerState = useState();
@@ -30,6 +30,50 @@ export const usePlayer = () => {
       }))
     }
 
+    //You could actually include both rotate and playerRotate in the same function,
+    //but it's best not to because at the end of the day, they will be doing seperate functions. 
+    //playerRotate will check the collision when rotating the tetromino.
+
+    //rotate will take in tetromino(matrix) and  direction. We want to rotate the tetromino.
+    const rotate = (matrix, dir) => {
+      //step one: convert the rows into columns(transpose)
+      const rotatedTetro = matrix.map((_,index) => 
+        matrix.map(col => col[index]),
+        ); 
+
+        //Reverse each row to get a rotated matrix(tetromino)
+        //we'll have to seperate into two different things. Becuase it depends what direction youre going. 
+        if (dir > 0) return rotatedTetro.map(row => row.reverse())
+        return rotatedTetro.reverse();
+      };
+
+    const playerRotate = (stage, dir) => {
+      //we don't want to mutate anything, so we'll make a copy of player. We'll need to make a deep clone.
+      //This makes us not work with a player in the state becuase we shouldn't mutate the state.
+      const clonedPlayer = JSON.parse(JSON.stringify(player));
+
+      clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+      const pos = clonedPlayer.pos.x;
+
+      let offset = 1;
+
+      while(checkCollision(clonedPlayer, stage, { x:0, y: 0})) {
+        clonedPlayer.pos.x += offset;
+
+        //this creates the ''back and forth movement'' with a tetromino
+        offset = -(offset + (offset > 0 ? 1 :-1 ))
+        if(offset > clonedPlayer.tetromino[0].length) {
+          rotate(clonedPlayer.tetromino, -dir);
+          clonedPlayer.pos.x = pos;
+          return;
+        }
+      }
+
+
+      setPlayer(clonedPlayer);
+
+    }
     const resetPlayer = useCallback(() => {
       setPlayer({
         pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
@@ -38,7 +82,7 @@ export const usePlayer = () => {
       })
     }, [])
   
-    return [player, updatePlayerPos, resetPlayer];
+    return [player, updatePlayerPos, resetPlayer, playerRotate];
   }
 //we are returning the player because we are importing the hook(usePlayer)
 //into the tetris component and thrn we are going to need the player 
